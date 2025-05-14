656d4a25408115788d893b6803fef1e6422b7300
ochkojs <ochkojs@gmail.com>
2025-05-14 09:43:05 +0800

    update

--- frontend/src/app/(auth)/_component/SignUp.tsx
+++ frontend/src/app/(auth)/_component/SignUp.tsx
@@ -3,14 +3,37 @@
 import { useState } from "react";
 import { SignUpUsernameCard } from "./SignUpUsernameCard";
 import { SignUpEmailPasswordCard } from "./SignUpEmailPasswordCard";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const SignUpPage = () => {
   const [step, setStep] = useState<number>(0);
   const [username, setUsername] = useState<string>("");
 
-  if (step === 0) {
-    return <SignUpUsernameCard setStep={setStep} setUsername={setUsername} />;
-  } else {
-    return <SignUpEmailPasswordCard username={username} />;
-  }
+  return (
+    <div className="relative w-full h-full">
+      <AnimatePresence mode="wait">
+        {step === 0 ? (
+          <motion.div
+            key="step-0"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 50 }}
+            transition={{ duration: 0.4 }}
+          >
+            <SignUpUsernameCard setStep={setStep} setUsername={setUsername} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="step-1"
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.4 }}
+          >
+            <SignUpEmailPasswordCard username={username} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 };
