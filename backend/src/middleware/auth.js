70d166431a127b156baba012a20181b0758c29ac
24LP8305 24LP8305 <24LP8305@G302-CC0118.local>
2025-05-22 12:33:29 +0800

    update

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,5 +1,5 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-const secret_key = process.env.SECRET_KEY;
+// const secret_key = process.env.SECRET_KEY;
 
-export const verifyToken = (req, res, next) => {};
+// export const verifyToken = (req, res, next) => {};
